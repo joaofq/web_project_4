cb23d49fcb520e77298fffd150e406b994634a3a
João Fernando <joaofq@hotmail.com>
2022-06-07 21:40:51 -0300

    Add addCards function

--- script/script.js
+++ script/script.js
@@ -1,38 +1,74 @@
 const editButton = document.querySelector(".editbutton");
-const closeButton = document.querySelector(".popup__close-icon");
-const popup = document.querySelector(".popup");
+const closeButton = document.querySelectorAll(".popup__close-icon");
+const popupEditProfile = document.querySelector(".popup-editprofile");
 const inputName = document.querySelector(".popup__input-name");
 const inputAbout = document.querySelector(".popup__input-about");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
-const popup__button = document.querySelector(".popup__button");
-const likeButton = document.querySelectorAll(".likebutton");
+const saveButtonEdit = document.querySelector(".popup__button-edit");
+const saveButtonAddCard = document.querySelector(".popup__button-addcard");
 const cardTemplate = document.querySelector(".cardTemplate").content;
 const elements = document.querySelector(".elements");
+const addButton = document.querySelector(".addbutton");
+const popupAddCard = document.querySelector(".popup-addcard");
+const inputCardTitle = document.querySelector(".popup__input-title");
+const inputCardLink = document.querySelector(".popup__input-link");
+
+//1 ABERTURA DE POPUPS
+
+//1.2. Popup edit profile - puxa dados iniciais
 
 function callPopup() {
-  popup.classList.toggle("popup_opened");
+  popupEditProfile.classList.add("popup_opened");
   inputName.value = profileName.textContent;
   inputAbout.value = profileDescription.textContent;
 }
 
-function savePopup(evt) {
+editButton.addEventListener("click", callPopup); //Jogar pro final
+
+//1.3. Popup addCard
+
+function callPopupAddCard() {
+  popupAddCard.classList.add("popup_opened");
+}
+
+addButton.addEventListener("click", callPopupAddCard); //Jogar pro final
+
+//2 FECHA AMBOS OS POPUPS
+
+closeButton.forEach(function (item) {
+  item.addEventListener("click", function closePopup() {
+    popupAddCard.classList.remove("popup_opened");
+    popupEditProfile.classList.remove("popup_opened");
+  });
+});
+
+//3 SALVA OS POPUPS
+
+saveButtonEdit.addEventListener("click", function (evt) {
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputAbout.value;
-  popup.classList.toggle("popup_opened");
+  popupEditProfile.classList.remove("popup_opened");
   evt.preventDefault();
+});
+
+saveButtonAddCard.addEventListener("click", addCard);
+
+function addCard() {
+  let cardTitle = inputCardTitle.value;
+  let cardLink = inputCardLink.value;
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__text").textContent = cardTitle;
+  cardElement.querySelector(".card__image").src = cardLink;
+  cardElement.querySelector(".card__image").alt = "Image of " + cardTitle;
+  elements.prepend(cardElement);
+  popupAddCard.classList.remove("popup_opened");
+  inputCardLink.value = "";
+  inputCardTitle.value = "";
+  event.preventDefault();
 }
 
-for (let i = 0; i < likeButton.length; i++) {
-  likeButton[i].addEventListener("click", callLikeButton);
-  function callLikeButton() {
-    likeButton[i].classList.toggle("likebutton_active");
-  }
-}
-
-editButton.addEventListener("click", callPopup);
-closeButton.addEventListener("click", callPopup);
-popup__button.addEventListener("click", savePopup);
+//4. GERA OS 6 CARDS INICIAIS
 
 const initialCards = [
   {
@@ -62,8 +98,25 @@ const initialCards = [
 ];
 
 initialCards.map(function (item) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   cardElement.querySelector(".card__text").textContent = item.name;
   cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__image").alt = "Image of " + item.name;
   elements.append(cardElement);
 });
+
+//5. BOTÃO CURTIR
+
+//const likeButton = document.querySelectorAll(".likebutton");
+
+/*function liked() {
+  item.classList.toggle("likebutton_active");
+}
+
+
+/*for (let i = 0; i < likeButton.length; i++) {
+  likeButton[i].addEventListener("click", callLikeButton);
+  function callLikeButton() {
+    likeButton[i].classList.toggle("likebutton_active");
+  }
+}*/
