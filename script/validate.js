0201f606fa451993eada4771eab299904af881a1
João Fernando <joaofq@hotmail.com>
2022-06-22 15:15:13 -0300

    bugfix BEM metod

--- script/validate.js
+++ script/validate.js
@@ -76,6 +76,6 @@ enableValidation({
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
   inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
+  inputErrorClass: "popup__error",
   errorClass: "popup__error_visible",
 });
